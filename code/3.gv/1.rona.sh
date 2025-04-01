03e39394d26f0b9d2e8132dd3d40307876f312ec
Yakun Wang <126683611+linnanlab@users.noreply.github.com>
2025-04-01 19:55:34 +0800

    Create 1.rona.sh

---
+++ code/3.gv/1.rona.sh
@@ -0,0 +1,5 @@
+pyRona lfmm -pc ade19_cur.txt -fc /public1/linnan/wyk/ade/mod/gf/1264160.txt -out ./pop1264160.txtres.pdf -P 0.01 -assoc core2.pvalue.csv  -geno core.lfmm 
+pyRona lfmm -pc ade19_cur.txt -fc /public1/linnan/wyk/ade/mod/gf/1266180.txt -out ./1266180.txtres.pdf   -P 0.01 -assoc core2.pvalue.csv  -geno core.lfmm 
+pyRona lfmm -pc ade19_cur.txt -fc /public1/linnan/wyk/ade/mod/gf/5854160.txt -out ./5854160.txtres.pdf -P 0.01 -assoc core2.pvalue.csv  -geno core.lfmm
+pyRona lfmm -pc ade19_cur.txt -fc /public1/linnan/wyk/ade/mod/gf/5856180.txt -out ./5856180.txtres.pdf -P 0.01 -assoc core2.pvalue.csv  -geno core.lfmm
+pyRona lfmm -pc ade19_cur.txt -fc /public1/linnan/wyk/ade/mod/gf/5854160.txt -out ./5854160.txtres.pdf -P 0.01 -assoc core2.pvalue.csv  -geno core.lfmm
